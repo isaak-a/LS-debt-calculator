c94163580ec19e1b6b5439ee7e6bd7a24f60a2fb
isaak-a <isaakranderson@gmail.com>
2021-07-15 17:53:40 -0400

    Meta tags for SEO

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import dash
 import dash_bootstrap_components as dbc
 
+site_description = (
+    "Law School Debt Calculator - How in the hole will you "
+    "be after law school? Quickly estimate your post-law "
+    "school debt to make an informed decision about which school to choose. "
+)
+
+site_keywords = "law school,law school debt,law school calculator,law school data"
 
 def get_dash_app():
     external_stylesheets = [dbc.themes.BOOTSTRAP]
     meta_tags=[
-        {"name": "viewport", "content": "width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1"}
+        {
+            "name": "viewport", 
+            "content": "width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1",
+            "description": site_description,
+            "keywords": site_keywords
+        }
     ]
 
     app = dash.Dash(
