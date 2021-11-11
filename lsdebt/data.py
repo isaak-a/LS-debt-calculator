5761bcc9c8a01a17a98724336862925dffc09f52
isaak-a <isaakranderson@gmail.com>
2021-11-11 11:43:24 -0500

    Add logging and configuration for gunicorn

--- lsdebt/data.py
+++ lsdebt/data.py
@@ -1,19 +1,28 @@
+import logging
+import os
+
 import pandas as pd
 import psycopg2
 
-import os
+
+log = logging.getLogger('app.db')
 
 
 def get_db_conn():
     conn_string = os.environ["DATABASE_URL"]
-    return psycopg2.connect(conn_string)
+
+    log.info("Connecting to PostGres")
+    connect = psycopg2.connect(conn_string)
+    
+    return connect
+
 
 def get_homepage_data():
     db_conn = get_db_conn()
 
+    log.info("Fetching homepage data")
     query = """
         SELECT *
         FROM debt_calc_user.school_stats
     """
-
     return pd.read_sql(query, db_conn).set_index("school")
--- lsdebt/index.py
+++ lsdebt/index.py
@@ -1,21 +1,31 @@
+import logging
+
 from app import get_dash_app
-from pages import homepage
+import pages.homepage as hp
 
 
 def create_app():
     dash_app = get_dash_app()
 
-    dash_app.layout = homepage.layout
+    dash_app.layout = hp.layout
     dash_app.title = "Law School Debt Calculator"
 
-    homepage.deploy_homepage_callbacks(dash_app)
+    hp.deploy_homepage_callbacks(dash_app)
 
     return dash_app
 
 app = create_app()
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+    app.logger.info("Loaded Gunicorn logger and handlers")
+
 server = app.server
 
 if __name__ == '__main__':
+    logging.basicConfig(level='INFO')
     app.run_server(
         debug=True,
         dev_tools_ui=True,
